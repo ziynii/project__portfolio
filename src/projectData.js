fe2f1a3410562be8b8fe45fe151467dc99daa4df
Jiyeon <ziyeon.ivi@gmail.com>
2024-11-29 17:52:15 +0900

    🚧 Add project data

---
+++ src/projectData.js
@@ -0,0 +1,207 @@
+export default [
+  {
+    id: 1,
+    title: '백예린 아티스트 페이지',
+    url: {
+      github: 'https://github.com/ziynii/project__yerin-artist-page',
+      figma:
+        'https://www.figma.com/design/4x8BEvaUi7pxeEFQxgMeaZ/%EC%95%84D%8B%B0%EC%8A%A4%ED%8A%B8-%ED%8E%98%EC%9D%B4%EC%A7%80?node-id=2%3A3&t=pJ4Xtbt8cAvEm93Q-1',
+      web: 'https://main--yerin-artist-page.netlify.app/',
+    },
+    type: 'responsive',
+    description:
+      '가수 백예린의 아티스트 소개 페이지를 디자인하고 반응형으로 제작했습니다.\n피그마를 사용해 디자인 작업을 진행했고 데스크탑 모바일 등 다양한 사용자 환경에 맞춘 레이아웃을 구성하였습니다.\nhtml로 시멘틱한 구조를 설계하고 scss를 통해 스타일을 지정했으며 jquery와 다양한 라이브러리를 사용해 동적인 기능을 추가하였습니다. ',
+    stack: ['html', 'scss', 'javascript'],
+    keyFeatures: [
+      {
+        name: 'scss',
+        description:
+          'SCSS를 사용하면서 코드의 재사용성과 유지보수성을 크게 향상시켰습니다. 공통 스타일을 관리하기 위해 mixin과 function을 적극 활용하여 중복되는 코드를 줄이고, 일관된 디자인을 구현할 수 있었습니다. 특히, mixin은 반복되는 스타일을 간편하게 적용할 수 있어 개발 속도를 높여주었습니다. 또한, function을 사용해 계산된 값을 동적으로 적용함으로써 코드의 유연성을 더했습니다.',
+      },
+      {
+        name: 'jquery',
+        description:
+          ' jQuery를 사용하여 DOM 조작과 이벤트 핸들링을 간편하게 구현했습니다. jQuery의 직관적인 API 덕분에 복잡한 JavaScript 코드를 간결하게 작성할 수 있었고, 크로스브라우징 이슈를 손쉽게 해결할 수 있었습니다.',
+      },
+    ],
+    pages: ['메인페이지'],
+  },
+  {
+    id: 2,
+    title: '반스',
+    url: {
+      github: 'https://github.com/ziynii/project__vans-redesign',
+      figma:
+        'https://www.figma.com/design/EGz48HTLhn7Pxikrl1fNpn/%EB%B0%98%EC%8A%A4-%EB%A6%AC%EB%94%94%EC%9E%90%EC%9D%B8?node-id=0%3A1&t=CaZUU5CBbuMKq1IA-1',
+      web: 'https://main--vans-redesign.netlify.app/',
+    },
+    type: 'responsive',
+    description:
+      '반스 웹사이트를 리디자인한 후 반응형으로 제작했습니다. Figma를 사용해 전체적인 디자인 작업을 진행했습니다. HTML을 이용하여 시멘틱한 구조를 잡았습니다. JavaScript와 jQuery를 활용해 다양한 동적 기능을 추가했습니다. 이 과정에서 웹사이트가 다양한 기기에서 원활히 작동하도록 최적화했습니다.',
+    stack: ['html', 'scss', 'javascript'],
+    keyFeatures: [
+      {
+        name: 'scss',
+        description:
+          'SCSS를 사용하면서 코드의 재사용성과 유지보수성을 크게 향상시켰습니다. 공통 스타일을 관리하기 위해 mixin과 function을 적극 활용하여 중복되는 코드를 줄이고, 일관된 디자인을 구현할 수 있었습니다. 특히, mixin은 반복되는 스타일을 간편하게 적용할 수 있어 개발 속도를 높여주었습니다. 또한, function을 사용해 계산된 값을 동적으로 적용함으로써 코드의 유연성을 더했습니다.',
+      },
+      {
+        name: 'jquery',
+        description:
+          ' jQuery를 사용하여 DOM 조작과 이벤트 핸들링을 간편하게 구현했습니다. jQuery의 직관적인 API 덕분에 복잡한 JavaScript 코드를 간결하게 작성할 수 있었고, 크로스브라우징 이슈를 손쉽게 해결할 수 있었습니다.',
+      },
+    ],
+    pages: ['메인페이지', '상품페이지'],
+  },
+  {
+    id: 3,
+    title: '포인핸드',
+    url: {
+      github: 'https://github.com/ziynii/project__pawinhand',
+      figma:
+        'https://www.figma.com/design/jOnXPv0nKhz9CcylSAHJ6c/%ED%8F%AC%EC%9D%B8%ED%95%B8%EB%93%9C-%EB%A6%AC%EB%94%94%EC%9E%90%EC%9D%B8?node-id=1%3A2&t=CjndIdzJQRBBm5Yu-1',
+      web: 'https://main--pawin-hand-redesign.netlify.app/',
+    },
+    type: 'responsive',
+    description:
+      '포인핸드 웹사이트를 리디자인하고 반응형으로 제작했습니다. Figma를 사용하여 디자인 작업을 진행했고, HTML로 시멘틱한 구조를 잡았습니다. JavaScript와 jQuery를 사용하여 동적인 기능을 추가했습니다. 최종 결과물은 다양한 기기에서 잘 작동하도록 최적화되었습니다.',
+    stack: ['html', 'scss', 'javascript'],
+    keyFeatures: [
+      {
+        name: 'scss',
+        description:
+          'SCSS를 사용하면서 코드의 재사용성과 유지보수성을 크게 향상시켰습니다. 공통 스타일을 관리하기 위해 mixin과 function을 적극 활용하여 중복되는 코드를 줄이고, 일관된 디자인을 구현할 수 있었습니다. 특히, mixin은 반복되는 스타일을 간편하게 적용할 수 있어 개발 속도를 높여주었습니다. 또한, function을 사용해 계산된 값을 동적으로 적용함으로써 코드의 유연성을 더했습니다.',
+      },
+      {
+        name: 'jquery',
+        description:
+          ' jQuery를 사용하여 DOM 조작과 이벤트 핸들링을 간편하게 구현했습니다. jQuery의 직관적인 API 덕분에 복잡한 JavaScript 코드를 간결하게 작성할 수 있었고, 크로스브라우징 이슈를 손쉽게 해결할 수 있었습니다.',
+      },
+    ],
+    pages: ['메인페이지'],
+  },
+
+  {
+    id: 4,
+    title: '서울도서관',
+    url: {
+      github: 'https://github.com/ziynii/project__seoul-library-redesign',
+      figma:
+        'https://www.figma.com/design/FgWUVOauYI3mdfF850qr9r/%EC%84%9C%EC%9A%B8%EB%8F%84%EC%84%9C%EA%B4%80-%EB%A6%AC%EB%94%94%EC%9E%90%EC%9D%B8?node-id=0%3A1&t=guuIP2ZBGfPuDmn9-1',
+      web: 'https://main--seoul-library-redesign.netlify.app/',
+    },
+    type: 'responsive',
+    description:
+      '서울도서관 웹사이트를 리디자인한 후 반응형으로 제작했습니다. Figma를 사용해 디자인 작업을 세밀하게 진행했습니다. HTML을 활용하여 시멘틱한 구조를 잡았습니다. JavaScript를 이용해 다양한 동적 기능을 추가해 웹사이트의 인터랙티브한 요소를 강화했습니다. 다양한 환경의 기기에서 사이트를 이용할 수 있습니다. ',
+    stack: ['html', 'scss', 'javascript'],
+    keyFeatures: [
+      {
+        name: 'scss',
+        description:
+          'SCSS를 사용하면서 코드의 재사용성과 유지보수성을 크게 향상시켰습니다. 공통 스타일을 관리하기 위해 mixin과 function을 적극 활용하여 중복되는 코드를 줄이고, 일관된 디자인을 구현할 수 있었습니다. 특히, mixin은 반복되는 스타일을 간편하게 적용할 수 있어 개발 속도를 높여주었습니다. 또한, function을 사용해 계산된 값을 동적으로 적용함으로써 코드의 유연성을 더했습니다.',
+      },
+    ],
+    pages: ['메인페이지', '공지사항', '자주 물어보는 질문'],
+  },
+  {
+    id: 5,
+    title: '펫플',
+    url: {
+      github: 'https://github.com/ziynii/project__petple',
+      web: 'https://petple-b2f50.web.app/#/',
+    },
+    type: 'react',
+    description:
+      '반려동물을 키우는 사람들의 커뮤니티로 자유롭게 게시글을 작성할 수 있고, 목적에 맞는 소규모의 채팅방을 만들어 정보를 공유할 수 있습니다. firebase를 기반으로 database, storage, hosting, auth 등의 기능을 이용해 로그인과 회원가입, 글 작성 프로필 변경등의 기능을 구현했습니다.',
+    stack: ['react', 'scss', 'firebase'],
+    keyFeatures: [
+      {
+        name: 'scss',
+        description:
+          'SCSS를 사용하면서 코드의 재사용성과 유지보수성을 크게 향상시켰습니다. 공통 스타일을 관리하기 위해 mixin과 function을 적극 활용하여 중복되는 코드를 줄이고, 일관된 디자인을 구현할 수 있었습니다. 특히, mixin은 반복되는 스타일을 간편하게 적용할 수 있어 개발 속도를 높여주었습니다. 또한, function을 사용해 계산된 값을 동적으로 적용함으로써 코드의 유연성을 더했습니다.',
+      },
+      {
+        name: 'firebase',
+        description:
+          'firebase의 호스팅 기능과 Auth, Database를 활용하여 안정적이고 확장 가능한 웹 어플리케이션을 구축했습니다. Firebase Auth를 사용해 다양한 인증 방법(Google, Email 등)을 손쉽게 구현하여 사용자 관리의 효율성을 높였습니다. 또한, Firebase Realtime Database를 이용해 실시간 데이터 동기화와 관리를 가능하게 하여 사용자 경험을 개선했습니다. ',
+      },
+    ],
+    pages: ['메인페이지', '커뮤니티', '채팅', '프로필'],
+  },
+  {
+    id: 6,
+    title: '유튜브',
+    url: {
+      github: 'https://github.com/ziynii/project__youtube',
+      web: 'https://main--you-tube-redesign.netlify.app/',
+    },
+    type: 'react',
+    description:
+      'react로 제작한 youtube clone 웹 사이트 입니다. youtube api를 이용해 인기있는 동영상을 메인에 보여주고, 동영상을 검색할 수 있는 기능이 존재합니다. 스타일 작업에는 tailwind 라이브러리를 사용했고,반응형으로 제작해 모바일과 태블릿 pc에서도 사용이 가능합니다.',
+    stack: ['react', 'typescript', 'tailwindCSS', 'axios'],
+    keyFeatures: [
+      {
+        name: 'typescript',
+        description:
+          ' TypeScript를 사용하여 들어오는 데이터의 타입을 명확하게 정의하고, 코드 작성 시 자동완성 기능을 활용해 개발 효율성을 크게 높였습니다. TypeScript의 정적 타입 체크 덕분에 코드에서 발생할 수 있는 오류를 사전에 방지하고, 안정적인 애플리케이션을 구축할 수 있었습니다. 또한, 자동완성 기능은 코드의 가독성과 작성 속도를 향상시켜 생산성을 높이는 데 큰 도움이 되었습니다.',
+      },
+      {
+        name: 'tailwindCSS',
+        description:
+          ' TailwindCSS를 사용해 효율적인 스타일링을 구현했습니다. TailwindCSS의 유틸리티 클래스 기반 접근 방식 덕분에, 필요한 스타일을 빠르게 적용할 수 있었고, CSS 작성 시간을 크게 줄일 수 있었습니다. 또한, TailwindCSS는 불필요한 스타일시트 중복을 방지하고, 최적화된 빌드를 통해 퍼포먼스를 향상시켰습니다. 커스터마이징이 용이해 프로젝트 요구에 맞게 스타일을 세밀하게 조정할 수 있었으며, 코드의 가독성과 유지보수성 또한 크게 향상되었습니다. ',
+      },
+    ],
+    pages: ['메인페이지', '영상 재생 페이지'],
+  },
+  {
+    id: 7,
+    title: '아임일리터',
+    url: {
+      github:
+        'https://github.com/ziynii/project__im1l-renewal?tab=readme-ov-file',
+      web: 'https://ziynii.github.io/project__im1l-renewal/',
+    },
+    type: 'react',
+    description:
+      '아임일리터의 웹 사이트를 리디자인 후 반응형으로 제작했습니다. \nreact를 기반으로 작업했고 typescript를 사용해 타입에 대한 에러를 방지하고 가독성을 높였습니다. 스타일 작업은 stypled-component를 이용했고 framer-motion등의 라이브러리로 애니메이션을 구현했습니다.',
+    stack: ['react', 'typescript', 'styledComponent', 'framerMotion'],
+    keyFeatures: [
+      {
+        name: 'typescript',
+        description:
+          ' TypeScript를 사용하여 들어오는 데이터의 타입을 명확하게 정의하고, 코드 작성 시 자동완성 기능을 활용해 개발 효율성을 크게 높였습니다. TypeScript의 정적 타입 체크 덕분에 코드에서 발생할 수 있는 오류를 사전에 방지하고, 안정적인 애플리케이션을 구축할 수 있었습니다. 또한, 자동완성 기능은 코드의 가독성과 작성 속도를 향상시켜 생산성을 높이는 데 큰 도움이 되었습니다.',
+      },
+      {
+        name: 'styledComponents',
+        description:
+          ' Styled-Components를 사용하여 컴포넌트 기반의 스타일링을 효과적으로 구현했습니다. Styled-Components의 장점인 CSS-in-JS 방식을 통해 각 컴포넌트에 고유한 스타일을 직접 적용하고, 스타일 충돌 없이 유지보수할 수 있었습니다. 또한, props를 활용한 동적 스타일링이 가능해, 재사용 가능한 컴포넌트를 쉽게 만들 수 있었습니다. 코드와 스타일이 한곳에 모여 있어 가독성이 높아졌고, 개발자 경험도 개선되었습니다.',
+      },
+    ],
+    pages: ['메인페이지', '메뉴 소개'],
+  },
+  {
+    id: 8,
+    title: 'FSHOP',
+    url: {
+      github: 'https://github.com/ziynii/project__f-shop-v2',
+      web: 'https://main--extraordinary-semifreddo-05c739.netlify.app/',
+    },
+    type: 'react',
+    description:
+      'react기반의 프레임워크인 gatsby를 사용해 제작한 fake shop입니다. typescript로 타입에 대한 에러 방지와 가독성을 높였고, contentful를 이용해 데이터와 코드를 분리해 관리하였습니다.',
+    stack: ['react', 'gatsby', 'typescript', 'styledComponent', 'contentful'],
+    keyFeatures: [
+      {
+        name: 'styledComponents',
+        description:
+          ' Styled-Components를 사용하여 컴포넌트 기반의 스타일링을 효과적으로 구현했습니다. Styled-Components의 장점인 CSS-in-JS 방식을 통해 각 컴포넌트에 고유한 스타일을 직접 적용하고, 스타일 충돌 없이 유지보수할 수 있었습니다. 또한, props를 활용한 동적 스타일링이 가능해, 재사용 가능한 컴포넌트를 쉽게 만들 수 있었습니다. 코드와 스타일이 한곳에 모여 있어 가독성이 높아졌고, 개발자 경험도 개선되었습니다.',
+      },
+      {
+        name: 'contentful',
+        description:
+          ' Contentful을 사용하여 유연하고 효율적인 콘텐츠 관리를 구현했습니다. Contentful의 헤드리스 CMS 구조 덕분에 콘텐츠와 코드를 분리하여 관리할 수 있었고, API를 통해 다양한 콘텐츠를 손쉽게 불러올 수 있었습니다. Gatsby와 통합하여 실시간 콘텐츠 업데이트가 가능하면서도, 정적 사이트의 성능을 유지할 수 있어 결과적으로, Contentful은 콘텐츠 관리와 사이트 성능을 동시에 만족시키는 핵심 역할을 했습니다',
+      },
+    ],
+    pages: ['메인페이지', '상품 카테고리별 리스트', '상품페이지', '장바구니'],
+  },
+];
